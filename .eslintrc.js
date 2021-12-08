ae5f7a450de02a62bc05c330affbb50ba942f24a
gemerson <henryingtel@gmail.com>
2021-12-07 22:50:10 -0500

    feat: updating eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,25 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  rules: {
+    indent: ['error', 2, { SwitchCase: 1 }],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    semi: ['error', 'always'],
+    // 'no-empty-function': 'off',
+    // '@typescript-eslint/no-empty-function': 'off',
+    // '@typescript-eslint/no-explicit-any': 'off',
+    // '@typescript-eslint/explicit-module-boundary-types': ['off'],
+    // '@typescript-eslint/no-unsafe-assignment': 'off',
+    // '@typescript-eslint/no-unsafe-member-access': 'off',
+    // 'react/display-name': 'off',
+    // 'react/prop-types': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        'endOfLine': 'auto',
+        'no-inline-styles': true
+      }
+    ]
+  },
 };
